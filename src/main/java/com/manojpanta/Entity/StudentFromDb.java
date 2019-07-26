f0f592729142bcc87747b1bf2b2e40fa62e9efad
manojpanta <manojpanta95@gmail.com>
2019-07-26 16:26:09 -0600

    add student entity that is conneceted to students table in the db

---
+++ src/main/java/com/manojpanta/Entity/StudentFromDb.java
@@ -0,0 +1,44 @@
+package com.manojpanta.Entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name= "students")
+public class StudentFromDb {
+    @Id
+    private Long id;
+    private String name;
+    private int finalScore;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getFinalScore() {
+        return finalScore;
+    }
+
+    public void setFinalScore(int finalScore) {
+        this.finalScore = finalScore;
+    }
+
+    public StudentFromDb(long id, String name, int finalScore) {
+        this.id = id;
+        this.name = name;
+        this.finalScore = finalScore;
+    }
+}
