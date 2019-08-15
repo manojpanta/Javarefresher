9d63819ddb7da363f0b3936af1113d5b645b2022
manojpanta <manojpanta95@gmail.com>
2019-08-15 17:02:03 -0600

    use map to modify elements in the list and understand parallel and sequential steam

--- src/main/java/com/manojpanta/Entity/StreamApi.java
+++ src/main/java/com/manojpanta/Entity/StreamApi.java
@@ -7,6 +7,6 @@ import java.util.List;
 public class StreamApi{
     public  static  void main(String[] args) {
         List<String> mylist = Arrays.asList("a1", "a2", "a3", "b1", "c1", "c2", "c3");
-        mylist.stream().filter(s-> s.startsWith("c")).sorted().map(String::toUpperCase).forEach(System.out::println);
+        mylist.stream().map(s->s.concat(" Manoj Wrote this.")).map(String::toUpperCase).forEach(System.out::println);
     }
 }
